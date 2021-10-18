49dfaf4fc17889beb7b245acfc9800283927ae12
everAtWork <ever.at.work@gmail.com>
2021-10-18 19:17:40 +0300

    +getAll for devices @deviceController

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -20,7 +20,23 @@ class DeviceController {
         }
     }
     async getAll(req,res) {
-
+        const {brandId, typeId} = req.query
+        let devices;
+        if (!brandId && !typeId) {
+            devices = await Device.findAll()
+        } 
+        if (brandId && !typeId) {
+            devices = await Device.findAll({where: {brandId}})
+        } 
+        if (!brandId && typeId) {
+            devices = await Device.findAll({where: {typeId}})
+            
+        } 
+        if (brandId && typeId) {
+            devices = await Device.findAll({where: {brandId, typeId}})
+            
+        } 
+        return res.json(devices)
     }
     async getSingleDevice(req,res) {
 

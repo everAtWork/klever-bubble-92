191b6be71c6ac2fd240ba414c44aebf7865c6b6e
everAtWork <ever.at.work@gmail.com>
2021-10-18 19:20:45 +0300

    added page & limit to help impl pagination

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -20,20 +20,23 @@ class DeviceController {
         }
     }
     async getAll(req,res) {
-        const {brandId, typeId} = req.query
+        const {brandId, typeId, page, limit} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page * limit - limit
         let devices;
         if (!brandId && !typeId) {
-            devices = await Device.findAll()
+            devices = await Device.findAll({limit, offset})
         } 
         if (brandId && !typeId) {
-            devices = await Device.findAll({where: {brandId}})
+            devices = await Device.findAll({where: {brandId}, limit, offset})
         } 
         if (!brandId && typeId) {
-            devices = await Device.findAll({where: {typeId}})
+            devices = await Device.findAll({where: {typeId}, limit, offset})
             
         } 
         if (brandId && typeId) {
-            devices = await Device.findAll({where: {brandId, typeId}})
+            devices = await Device.findAll({where: {brandId, typeId}, limit, offset})
             
         } 
         return res.json(devices)
